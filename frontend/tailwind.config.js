b3996d67003779f93bcf033fb92d5baef1ac1729
itjockebark <itjockebark@gmail.com>
2025-11-04 19:40:33 +0100

    Installs tailwind Updates nuxt config and creates necessary files accordingly to the tailwind implementation

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './app/**/*.{vue,js,ts}',
+    './components/**/*.{vue,js,ts}',
+    './pages/**/*.{vue,js,ts}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
